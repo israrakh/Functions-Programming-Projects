8bd494e3eb1de3245c96e7eda54adc6fff9fdddb
Israr <israr.akh03@gmail.com>
2024-10-04 19:15:17 -0400

    Add project files.

---
+++ Functions Programming Projects.cpp
@@ -0,0 +1,9 @@
+// Functions Programming Projects.cpp : Contains two different programming projects(Properties of a Circle, and Inflation Trend)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "It Contains two different programming projects(Properties of a Circle, and Inflation Trend)" << endl;
+}
